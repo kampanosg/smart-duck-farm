350220952834e10334d067b5bbb28a27d649286c
George Kampanos <gkabanos@icloud.com>
2023-04-06 18:32:39 +0100

    basic mint

--- contracts/Duck.sol
+++ contracts/Duck.sol
@@ -11,13 +11,16 @@ import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 contract Duck is ERC721Upgradeable {
 
     using EnumerableSet for EnumerableSet.UintSet;
+    using Counters for Counters.Counter;
 
     string public baseURL;
     uint256 public mintFeeAmount;
     uint256 public constant maxSupply = 10000;
     mapping(uint256 => Duckling) public ducklings;
 
+    Counters.Counter private _tokenIdCounter;
     address private _owner;
+    address private _royaltiesAddr;
     mapping(address => EnumerableSet.UintSet) private _holderTokens;
 
     struct Duckling {
@@ -34,5 +37,30 @@ contract Duck is ERC721Upgradeable {
         mintFeeAmount = 1000000000000000000;
         baseURL = _baseURL;
         _owner = _nftOwner;
+        _royaltiesAddr = _nftOwner;
     }
+
+    function mint() public payable {
+        require(_tokenIdCounter.current() < maxSupply, "Max supply reached");
+        require(msg.value == mintFeeAmount, "Not enough fee");
+        payable(_royaltiesAddr).transfer(msg.value);
+
+        _tokenIdCounter.increment();
+        uint256 nextTokenId = _tokenIdCounter.current();
+        _safeMint(msg.sender, nextTokenId);
+        ducklings[nextTokenId] = Duckling(nextTokenId, msg.sender, msg.sender, 0, false, 0);
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _tokenIdCounter.current();
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721Upgradeable) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId) public view override(ERC721Upgradeable) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
 }
