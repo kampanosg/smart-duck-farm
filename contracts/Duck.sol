d889612f27ff57f4961686d6ffbf18bde421877e
George Kampanos <gkabanos@icloud.com>
2023-04-06 17:49:49 +0100

    some basic globals

--- contracts/Duck.sol
+++ contracts/Duck.sol
@@ -1,10 +1,19 @@
 // contracts/MyNFT.sol
 // SPDX-License-Identifier: MIT
+// a mutha duckin' upgradeable NFT contract
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Duck is ERC721 {
+contract Duck is ERC721Upgradeable {
+
+    address private _owner;
+
+    uint256 public mintFeeAmount;
+    uint256 public constant maxSupply = 10000;
+
     constructor() ERC721("Duck NFT", "DCK") {
     }
 }
