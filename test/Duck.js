880a076e3cd5f64dd2d3de3088ea06a18e554db6
George Kampanos <gkabanos@icloud.com>
2023-04-11 20:30:51 +0100

    checkpoint

--- test/Duck.js
+++ test/Duck.js
@@ -218,7 +218,7 @@ describe("Duck Contract - totalSupply", () => {
     });
 });
 
-describe("Duckling Contract - getDuckling", () => {
+describe("Duck Contract - getDuckling", () => {
     let duckContract = null;
     let owner = null;
     let other = null;
