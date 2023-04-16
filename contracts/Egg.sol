fc1d265f06929083dc9feaae50a7078f6419337a
George Kampanos <gkabanos@icloud.com>
2023-04-16 15:25:25 +0100

    calculate claimable eggs

--- contracts/Egg.sol
+++ contracts/Egg.sol
@@ -18,8 +18,18 @@ contract Egg is ERC20 {
     uint256 public LVL_BASE = 25;
     uint256 public LVL_RATE = 2;
     uint256 public COOLDOWN_RATE_SECS = 7200;
+    uint256 public EGGS_PER_DAY_PER_KG = 250000000000000000; // the number of $EGG per duckling per day per kg: 0.25 $egg per $duck / day / weight
+    uint256 public BASE_HOLDER_EGGS = 750000000000000000;    // the base number of $EGG per duckling: 0.75 $egg
+    uint256 public ONE_DAY_IN_SECS = 86400;
 
-    EnumerableSet.UintSet private _stakedDucks;
+    struct StakedDuckling {
+        uint256 weight;
+        uint256 lastTimeFarmedTs;
+        uint256 amountFed;
+        uint256 cooldownTime;
+    }
+
+    mapping(uint256 => StakedDuckling) public stakedDucklings;
 
     constructor(address _duckContractAddr) ERC20("Egg Token", "EGG") {
         DUCK_CONTRACT_ADDR = _duckContractAddr;
@@ -37,7 +47,13 @@ contract Egg is ERC20 {
 
     // calculate the amount of $EGG that is claimable from a duck, based on its weight
     function claimableEgg(uint256 tokenId) public view returns (uint256) {
-        
+        StakedDuckling memory stakedDuckling = stakedDucklings[tokenId];
+        if (stakedDuckling.weight == 0) {
+            return 0;
+        }
+        uint256 eggPerDay = ((EGGS_PER_DAY_PER_KG * (stakedDuckling.weight / 100)) + BASE_HOLDER_EGGS);
+        uint256 deltaSeconds = block.timestamp - stakedDuckling.lastTimeFarmedTs;
+        return deltaSeconds * (eggPerDay / ONE_DAY_IN_SECS);
     }
 
 }
