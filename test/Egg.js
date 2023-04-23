d3278f74ed8ce41c82f50a522ab6d12ccd97fa90
George Kampanos <gkabanos@icloud.com>
2023-04-23 16:20:01 +0100

    added tests for cooldown rate

--- test/Egg.js
+++ test/Egg.js
@@ -1,7 +1,7 @@
 const { assert, expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Egg Contract - feedLevellingRate", function () {
+describe("Egg Contract - feedLevellingRate", () => {
 
     let duckContract = null;
     let eggContract = null;
@@ -16,7 +16,7 @@ describe("Egg Contract - feedLevellingRate", function () {
         await eggContract.deployed();
     });
 
-    it("should return correct value based on duck weight", async function () {
+    it("should return correct value based on duck weight", async () => {
         let tests = [
             { duckKg: 0, expected: 0 },
             { duckKg: 100, expected: 25 },
@@ -33,6 +33,43 @@ describe("Egg Contract - feedLevellingRate", function () {
 
         for (let i = 0; i < tests.length; i++) {
             const result = await eggContract.feedLevellingRate(tests[i].duckKg);
+            assert.equal(result, tests[i].expected);
+        }
+    });
+});
+
+describe("Egg Contract - cooldownRate", () => {
+
+    let duckContract = null;
+    let eggContract = null;
+
+    beforeEach(async () => {
+        const Duck = await ethers.getContractFactory("Duck");
+        duckContract = await Duck.deploy();
+        await duckContract.deployed();
+
+        const Egg = await ethers.getContractFactory("Egg");
+        eggContract = await Egg.deploy(duckContract.address);
+        await eggContract.deployed();
+    });
+
+    it("should return correct value based on duck weight", async () => {
+        let tests = [
+            { duckKg: 0, expected: 0 },
+            { duckKg: 100, expected: 7200 },
+            { duckKg: 200, expected: 14400 },
+            { duckKg: 300, expected: 21600 },
+            { duckKg: 400, expected: 28800 },
+            { duckKg: 500, expected: 36000 },
+            { duckKg: 600, expected: 43200 },
+            { duckKg: 700, expected: 50400 },
+            { duckKg: 800, expected: 57600 },
+            { duckKg: 900, expected: 64800 },
+            { duckKg: 1000, expected: 72000 },
+        ]
+
+        for (let i = 0; i < tests.length; i++) {
+            const result = await eggContract.cooldownRate(tests[i].duckKg);
             assert.equal(result, tests[i].expected);
         }
     });
