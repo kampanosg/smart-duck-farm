550b87f754edc04783d4f65306e9decdf6963ad2
George Kampanos <gkabanos@icloud.com>
2023-05-06 03:29:04 +0100

    stake and update

--- contracts/Bread.sol
+++ contracts/Bread.sol
@@ -17,8 +17,42 @@ contract Bread is ERC20, Authorizable {
     address public DUCK_CONTRACT_ADDR;
     address public EGG_CONTRACT_ADDR;
 
+    struct Stake {
+        address user;
+        uint256 amount;
+        uint256 ts;
+    }
+
+    mapping(address => Stake) public stakes;
+
     constructor(address _duckContractAddr, address _eggContractAddr) ERC20("Bread Token", "BREAD") {
         DUCK_CONTRACT_ADDR = _duckContractAddr;
         EGG_CONTRACT_ADDR = _eggContractAddr;
     }
+
+    function stake(uint256 amount) external {
+        require(amount > 0, "no egg");
+        Egg eggContract = Egg(EGG_CONTRACT_ADDR);
+        uint256 available = eggContract.balanceOf(msg.sender);
+        require(available >= amount, "not enough egg");
+        Stake memory existingStake = stakes[msg.sender];
+        if (existingStake.amount > 0) {
+            uint256 claimable = claimableFeed(msg.sender);
+            _mint(msg.sender, claimable);
+            updateStake(msg.sender, existingStake.amount + amount);
+        } else {
+            updateStake(msg.sender, amount);
+        }
+        eggContract.burnEgg(msg.sender, amount);
+    }
+
+    function updateStake(address user, uint256 amount) internal {
+        Stake s = Stake {
+            user: user,
+            amount: amount,
+            ts: block.timestamp
+        };
+        stakes[user] = s;
+    }
+
 }
